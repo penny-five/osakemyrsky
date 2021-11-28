3f37f8e8362efcd0b793329a5af64018852ade8a
penny-five <lehton83@gmail.com>
2021-11-28 12:58:43 +0200

    remove leftovers from postgresql

--- packages/@osakemyrsky-server/typings/node-augs/index.d.ts
+++ packages/@osakemyrsky-server/typings/node-augs/index.d.ts
@@ -1,8 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NODE_ENV: "development" | "production";
-    PG_CONNECTION_STRING: string;
     PORT: string;
+    NODE_ENV: "development" | "production";
     FIRESTORE_HOST_URL?: string;
     FIRESTORE_HOST_PORT?: string;
     GOOGLE_CLIENT_ID: string;
