36311adcf11dd9b4d5fe52201f14dfcadcd2b5a9
penny-five <lehton83@gmail.com>
2021-12-25 13:18:29 +0200

    fix league table league name  truncation

--- packages/@osakemyrsky-client/src/components/league-browser/league-table.tsx
+++ packages/@osakemyrsky-client/src/components/league-browser/league-table.tsx
@@ -18,7 +18,7 @@ const LeagueTable = ({ leagues }: LeagueTableProps) => {
     <table className="flex flex-col w-full">
       <thead className="">
         <tr className="flex w-full font-bold text-gray-600 border-b-1 border-gray-400">
-          <th className="grow p-4 text-left">Liigan nimi</th>
+          <th className="w-[50%] grow p-4 text-left">Liigan nimi</th>
           <th className="w-[15%] p-4 text-left">Osallistujat</th>
           <th className="w-[20%] p-4 text-left">Aikataulu</th>
           <th className="w-[15%] p-4 text-left">Tilanne</th>
@@ -27,7 +27,7 @@ const LeagueTable = ({ leagues }: LeagueTableProps) => {
       <tbody className="flex flex-col w-full my-4 gap-4">
         {leagues.map(league => (
           <tr key={league.id} className="flex items-center w-full bg-gray-200 rounded-xl">
-            <td className="grow p-4 font-bold text-lg">
+            <td className="w-[50%] p-4 font-bold text-lg truncate">
               <Link
                 href={{
                   pathname: "/leagues/[id]",
@@ -35,10 +35,10 @@ const LeagueTable = ({ leagues }: LeagueTableProps) => {
                 }}
                 passHref
               >
-                <a className="hover:underline">{league.name}</a>
+                <a className=" hover:underline">{league.name}</a>
               </Link>
             </td>
-            <td className="p-4 w-[15%]">
+            <td className="w-[15%] p-4 ">
               <AvatarStack
                 pictures={league.members.map(member => ({
                   url: member.picture,
@@ -46,8 +46,8 @@ const LeagueTable = ({ leagues }: LeagueTableProps) => {
                 }))}
               />
             </td>
-            <td className="p-4 w-[20%] font-semibold text-base">{formatDayRange(league.startDate, league.endDate)}</td>
-            <td className="p-4 w-[15%]">
+            <td className="w-[20%] p-4 font-semibold text-base">{formatDayRange(league.startDate, league.endDate)}</td>
+            <td className="w-[15%] p-4">
               <LeagueStatusBadge status={league.status} />
             </td>
           </tr>
